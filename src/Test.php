af4457ac83d9c8077b4c28f1cb030d394a734ae5
Philip Gichuhi <pgichuhi@microsoft.com>
2022-03-01 17:23:59 +0300

    Update

--- src/Test.php
+++ src/Test.php
@@ -5,7 +5,8 @@ require_once "../vendor/autoload.php";
 use Microsoft\Kiota\Authentication\Oauth\ClientCredentialContext;
 use Microsoft\Kiota\Http\GuzzleRequestAdapter;
 use Microsoft\Kiota\Authentication\PhpLeagueAuthenticationProvider;
-use \Microsoft\Graph\GraphClient;
+use Microsoft\Graph\GraphClient;
+use Microsoft\Kiota\Http\KiotaClientFactory;
 
 define("CLIENT_ID", getenv("client_id"));
 define("TENANT_ID", getenv("test_tenantId"));
@@ -16,12 +17,23 @@ $tokenRequestContext = new ClientCredentialContext(
     CLIENT_ID,
     CLIENT_SECRET
 );
+
+$guzzleClient = KiotaClientFactory::createWithConfig([
+    'proxy' => 'http://localhost:8888',
+    'verify' => false
+]);
+
 $requestAdapter = new GuzzleRequestAdapter(
-    new PhpLeagueAuthenticationProvider($tokenRequestContext, ['https://graph.microsoft.com/.default'])
+    new PhpLeagueAuthenticationProvider($tokenRequestContext, ['https://graph.microsoft.com/.default']),
+    null,
+    null,
+    $guzzleClient
 );
+
 $graphClient = new GraphClient($requestAdapter);
 
 $response = $graphClient->usersById('pgichuhi@pgichuhi.onmicrosoft.com')->messages()->get()->wait();
+var_dump($response);
 foreach ($response as $message) {
     echo "From: {$message->getFrom()}";
     echo "Subject: {$message->getSubject()}";
