8c935601a7675cd451f40a8c2fd09e9c47ddf264
Philip Gichuhi <pgichuhi@microsoft.com>
2022-03-02 17:15:08 +0300

    Latest changes

---
+++ README.md
@@ -0,0 +1,5 @@
+## Set Up
+1. `git clone`
+2. Navigate into cloned repository directory
+3. `git submodule update --init`
+4. `composer install`
--- src/Test.php
+++ src/Test.php
@@ -9,17 +9,32 @@ use Microsoft\Kiota\Authentication\PhpLeagueAuthenticationProvider;
 use Microsoft\Graph\GraphClient;
 use Microsoft\Kiota\Http\KiotaClientFactory;
 use Microsoft\Graph\Models\Microsoft\Graph\Message;
+use Microsoft\Graph\Models\Microsoft\Graph\ItemBody;
+use Microsoft\Graph\Models\Microsoft\Graph\BodyType;
+use Microsoft\Graph\Models\Microsoft\Graph\EmailAddress;
+use Microsoft\Graph\Models\Microsoft\Graph\Recipient;
+use Microsoft\Graph\Models\Microsoft\Graph\Importance;
 
 define("CLIENT_ID", getenv("client_id"));
 define("TENANT_ID", getenv("test_tenantId"));
 define("CLIENT_SECRET", getenv("test_secret"));
-const USER_ID = 'pgichuhi@pgichuhi.onmicrosoft.com';
+const USER_ID = 'pgichuhi@sk7xg.onmicrosoft.com';
 
 $tokenRequestContext = new ClientCredentialContext(
-    TENANT_ID,
-    CLIENT_ID,
-    CLIENT_SECRET
+    'a4006d64-dade-442d-a695-5934f14a4150',
+    'c2c8e135-e3bd-4870-9092-5eba03dd9102',
+    'Exh7Q~NLoJjNSt_UZ6Z..cePwf4tSwsXu-RPj'
 );
+
+$mockResponse = [
+    function (\Psr\Http\Message\RequestInterface $request) {
+        $jsonBody = $request->getBody()->getContents();
+        $decoded = json_decode($jsonBody);
+        return new \GuzzleHttp\Psr7\Response(200);
+    }
+];
+
+//$guzzleClient = KiotaClientFactory::createWithMiddleware(new \GuzzleHttp\HandlerStack(new \GuzzleHttp\Handler\MockHandler($mockResponse)));
 
 $guzzleClient = KiotaClientFactory::createWithConfig([
     'proxy' => 'http://localhost:8888',
@@ -35,34 +50,37 @@ $requestAdapter = new GuzzleRequestAdapter(
 
 $graphClient = new GraphClient($requestAdapter);
 
-//// GET collection of messages
-//$messages = $graphClient->usersById(USER_ID)->messages()->get()->wait();
-//foreach ($messages->getValue() as $message) {
-//    printMessage($message);
-//}
-//
-//// GET item
-//$sampleMessageId = $messages->getValue()[0]->getId();
-//$message = $graphClient->usersById(USER_ID)->messagesById($sampleMessageId)->get()->wait();
-//printMessage($message);
+// GET collection of messages
+$messages = $graphClient->usersById(USER_ID)->messages()->get()->wait();
+foreach ($messages->getValue() as $message) {
+    printMessage($message);
+}
+
+// GET item
+$sampleMessageId = $messages->getValue()[0]->getId();
+$message = $graphClient->usersById(USER_ID)->messagesById($sampleMessageId)->get()->wait();
+printMessage($message);
+
+//// DELETE item
+//$response = $graphClient->usersById(USER_ID)->messagesById($sampleMessageId)->delete()->wait();
+
 
 // POST
-$body = new \Microsoft\Graph\Models\Microsoft\Graph\ItemBody();
+$body = new ItemBody();
 $body->setContent("They were awesome");
+$body->setContentType(new BodyType(BodyType::TEXT));
 
-$recipient = new \Microsoft\Graph\Models\Microsoft\Graph\Recipient();
-$email = new \Microsoft\Graph\Models\Microsoft\Graph\EmailAddress();
+$email = new EmailAddress();
 $email->setAddress("Test@contoso.onmicrosoft.com");
+$recipient = new Recipient();
 $recipient->setEmailAddress($email);
-$recipients = [
-    $recipient
-];
 
 $message = new Message();
 $message->setSubject("KIOTA DEMO SUBJECT");
-$message->setImportance(new \Microsoft\Graph\Models\Microsoft\Graph\Importance('low'));
+$message->setImportance(new Importance(Importance::LOW));
 $message->setBody($body);
-$message->setToRecipients($recipients);
+$message->setToRecipients([$recipient]);
 
-$response = $graphClient->usersById(USER_ID)->messages()->post($message);
+$response = $graphClient->usersById(USER_ID)->messages()->post($message)->wait();
+var_dump($response);
 printMessage($response);
