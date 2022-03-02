c4873d02212ed80e2e99ec53018630ba31aefd1d
Philip Gichuhi <pgichuhi@microsoft.com>
2022-03-02 18:45:32 +0300

    Demo ready

--- src/Demo.php
+++ src/Demo.php
@@ -15,9 +15,6 @@ use Microsoft\Graph\Models\Microsoft\Graph\EmailAddress;
 use Microsoft\Graph\Models\Microsoft\Graph\Recipient;
 use Microsoft\Graph\Models\Microsoft\Graph\Importance;
 
-define("CLIENT_ID", getenv("client_id"));
-define("TENANT_ID", getenv("test_tenantId"));
-define("CLIENT_SECRET", getenv("test_secret"));
 const USER_ID = 'pgichuhi@sk7xg.onmicrosoft.com';
 
 $tokenRequestContext = new ClientCredentialContext(
@@ -25,16 +22,6 @@ $tokenRequestContext = new ClientCredentialContext(
     'c2c8e135-e3bd-4870-9092-5eba03dd9102',
     'Exh7Q~NLoJjNSt_UZ6Z..cePwf4tSwsXu-RPj'
 );
-
-$mockResponse = [
-    function (\Psr\Http\Message\RequestInterface $request) {
-        $jsonBody = $request->getBody()->getContents();
-        $decoded = json_decode($jsonBody);
-        return new \GuzzleHttp\Psr7\Response(200);
-    }
-];
-
-//$guzzleClient = KiotaClientFactory::createWithMiddleware(new \GuzzleHttp\HandlerStack(new \GuzzleHttp\Handler\MockHandler($mockResponse)));
 
 $guzzleClient = KiotaClientFactory::createWithConfig([
     'proxy' => 'http://localhost:8888',
@@ -61,10 +48,6 @@ $sampleMessageId = $messages->getValue()[0]->getId();
 $message = $graphClient->usersById(USER_ID)->messagesById($sampleMessageId)->get()->wait();
 printMessage($message);
 
-//// DELETE item
-//$response = $graphClient->usersById(USER_ID)->messagesById($sampleMessageId)->delete()->wait();
-
-
 // POST
 $body = new ItemBody();
 $body->setContent("They were awesome");
@@ -82,5 +65,4 @@ $message->setBody($body);
 $message->setToRecipients([$recipient]);
 
 $response = $graphClient->usersById(USER_ID)->messages()->post($message)->wait();
-var_dump($response);
 printMessage($response);
--- src/DemoUtils.php
+++ src/DemoUtils.php
@@ -1,9 +1,20 @@
 <?php
 
-function printMessage($message)
+use Microsoft\Graph\Models\Microsoft\Graph\Message;
+
+function printMessage(Message $message)
 {
     echo "Id: {$message->getId()}\n";
-    $from = $message->getFrom()->getEmailAddress();
-    echo "From: {$from->getName()} <{$from->getAddress()}>\n";
+    if ($message->getFrom()) {
+        $from = $message->getFrom()->getEmailAddress();
+        echo "From: {$from->getName()} <{$from->getAddress()}>\n";
+    }
+    if ($message->getToRecipients()) {
+        echo "Recipients: ";
+        foreach ($message->getToRecipients() as $recipient) {
+            echo "{$recipient->getEmailAddress()->getName()} <{$recipient->getEmailAddress()->getAddress()}>}, ";
+        }
+        echo "\n";
+    }
     echo "Subject: {$message->getSubject()}\n\n";
 }
