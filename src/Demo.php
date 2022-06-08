96103c7faa976d8f251cd5ed2c5354724550f0d0
Philip Gichuhi <pgichuhi@microsoft.com>
2022-06-08 15:27:25 +0300

    Update dependency config

--- src/Demo.php
+++ src/Demo.php
@@ -1,19 +1,10 @@
 <?php
 
+use Microsoft\Graph\GraphRequestAdapter;
+use Microsoft\Kiota\Authentication\Oauth\ClientCredentialContext;
+
 require_once "../vendor/autoload.php";
 require_once "./DemoUtils.php";
-
-use Microsoft\Kiota\Authentication\Oauth\ClientCredentialContext;
-use Microsoft\Kiota\Http\GuzzleRequestAdapter;
-use Microsoft\Kiota\Authentication\PhpLeagueAuthenticationProvider;
-use Microsoft\Graph\GraphClient;
-use Microsoft\Kiota\Http\KiotaClientFactory;
-use Microsoft\Graph\Models\Microsoft\Graph\Message;
-use Microsoft\Graph\Models\Microsoft\Graph\ItemBody;
-use Microsoft\Graph\Models\Microsoft\Graph\BodyType;
-use Microsoft\Graph\Models\Microsoft\Graph\EmailAddress;
-use Microsoft\Graph\Models\Microsoft\Graph\Recipient;
-use Microsoft\Graph\Models\Microsoft\Graph\Importance;
 
 const USER_ID = 'pgichuhi@sk7xg.onmicrosoft.com';
 
@@ -23,11 +14,9 @@ $tokenRequestContext = new ClientCredentialContext(
     'Exh7Q~NLoJjNSt_UZ6Z..cePwf4tSwsXu-RPj'
 );
 
-$requestAdapter = new GuzzleRequestAdapter(
-    new PhpLeagueAuthenticationProvider($tokenRequestContext, ['https://graph.microsoft.com/.default']),
-);
+$requestAdapter = GraphRequestAdapter::createWithTokenRequestContext($tokenRequestContext);
 
-$graphClient = new GraphClient($requestAdapter);
+$graphClient = new
 
 // GET collection of messages
 $messages = $graphClient->usersById(USER_ID)->messages()->get()->wait();
@@ -35,26 +24,26 @@ foreach ($messages->getValue() as $message) {
     printMessage($message);
 }
 
-// GET item
-$sampleMessageId = $messages->getValue()[0]->getId();
-$message = $graphClient->usersById(USER_ID)->messagesById($sampleMessageId)->get()->wait();
-printMessage($message);
-
-// POST
-$body = new ItemBody();
-$body->setContent("They were awesome");
-$body->setContentType(new BodyType(BodyType::TEXT));
-
-$email = new EmailAddress();
-$email->setAddress("Test@contoso.onmicrosoft.com");
-$recipient = new Recipient();
-$recipient->setEmailAddress($email);
-
-$message = new Message();
-$message->setSubject("KIOTA DEMO SUBJECT");
-$message->setImportance(new Importance(Importance::LOW));
-$message->setBody($body);
-$message->setToRecipients([$recipient]);
-
-$response = $graphClient->usersById(USER_ID)->messages()->post($message)->wait();
-printMessage($response);
+//// GET item
+//$sampleMessageId = $messages->getValue()[0]->getId();
+//$message = $graphClient->usersById(USER_ID)->messagesById($sampleMessageId)->get()->wait();
+//printMessage($message);
+//
+//// POST
+//$body = new ItemBody();
+//$body->setContent("They were awesome");
+//$body->setContentType(new BodyType(BodyType::TEXT));
+//
+//$email = new EmailAddress();
+//$email->setAddress("Test@contoso.onmicrosoft.com");
+//$recipient = new Recipient();
+//$recipient->setEmailAddress($email);
+//
+//$message = new Message();
+//$message->setSubject("KIOTA DEMO SUBJECT");
+//$message->setImportance(new Importance(Importance::LOW));
+//$message->setBody($body);
+//$message->setToRecipients([$recipient]);
+//
+//$response = $graphClient->usersById(USER_ID)->messages()->post($message)->wait();
+//printMessage($response);
