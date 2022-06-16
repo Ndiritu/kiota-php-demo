f05cc3e6ffe745d958d7f95f14624fe685361c63
Philip Gichuhi <pgichuhi@microsoft.com>
2022-06-16 12:49:44 +0300

    Plain text serialization sample

--- src/Demo.php
+++ src/Demo.php
@@ -2,6 +2,9 @@
 
 namespace Kiota\Demo;
 
+use GuzzleHttp\MessageFormatter;
+use GuzzleHttp\Middleware;
+use Microsoft\Graph\Core\GraphClientFactory;
 use Microsoft\Graph\Generated\Models\BodyType;
 use Microsoft\Graph\Generated\Models\EmailAddress;
 use Microsoft\Graph\Generated\Models\Importance;
@@ -10,7 +13,11 @@ use Microsoft\Graph\GraphRequestAdapter;
 use Microsoft\Graph\GraphServiceClient;
 use Microsoft\Graph\Generated\Models\Message;
 use Microsoft\Graph\Generated\Models\Recipient;
+use Microsoft\Graph\Generated\Users\Item\Messages\MessagesRequestBuilderGetQueryParameters;
+use Microsoft\Graph\Generated\Users\Item\Messages\MessagesRequestBuilderGetRequestConfiguration;
 use Microsoft\Kiota\Authentication\Oauth\ClientCredentialContext;
+use Monolog\Handler\StreamHandler;
+use Monolog\Logger;
 
 require_once "./vendor/autoload.php";
 
@@ -39,23 +46,32 @@ $tokenRequestContext = new ClientCredentialContext(
     'Exh7Q~NLoJjNSt_UZ6Z..cePwf4tSwsXu-RPj'
 );
 
-$requestAdapter = GraphRequestAdapter::createWithTokenRequestContext($tokenRequestContext, ['https://graph.microsoft.com/.default']);
+$logMessageTemplate = "{method}... {uri}\n\n\n{code}...\n";
+$messageFormatter = new MessageFormatter($logMessageTemplate);
 
+$log = new Logger('Demo');
+$log->pushHandler(new StreamHandler('php://stdout'));
+
+$middleware = GraphClientFactory::getDefaultHandlerStack();
+$middleware->push(Middleware::log($log, $messageFormatter));
+$httpClient = GraphClientFactory::createWithMiddleware($middleware);
+
+$requestAdapter = GraphRequestAdapter::withTokenRequestContext($tokenRequestContext, ['https://graph.microsoft.com/.default'])::withHttpClient($httpClient);
 $graphClient = new GraphServiceClient($requestAdapter);
 
 // GET collection of messages
-$messages = $graphClient->usersById(USER_ID)->messages()->get()->wait();
-echo "Fetched ".sizeof($messages->getValue())." messages\n\n";
+// $messages = $graphClient->usersById(USER_ID)->messages()->get()->wait();
+// echo "Fetched ".sizeof($messages->getValue())." messages\n\n";
 // foreach ($messages->getValue() as $message) {
 //     printMessage($message);
 // }
 
 // GET item
-$sampleMessageId = $messages->getValue()[0]->getId();
-echo "Sample Message ID: {$sampleMessageId}";
-$message = $graphClient->usersById(USER_ID)->messagesById($sampleMessageId)->get()->wait();
-echo "\n\n********************** GET ITEM **********************************\n\n";
-var_dump($message);
+// $sampleMessageId = $messages->getValue()[0]->getId();
+// echo "Sample Message ID: {$sampleMessageId}\n\n";
+// $message = $graphClient->usersById(USER_ID)->messagesById($sampleMessageId)->get()->wait();
+// echo "\n\n********************** GET ITEM **********************************\n\n";
+// var_dump($message);
 // printMessage($message);
 
 // POST
@@ -82,14 +98,32 @@ var_dump($message);
 
 
 // PATCH
-$updatedMsg = new Message();
-$updatedMsg->setSubject("Updated Subject!");
-$updatedMsg = $graphClient->usersById(USER_ID)->messagesById($sampleMessageId)->patch($updatedMsg)->wait();
-echo "\n\n********************* PATCH ITEM ****************************\n\n";
-var_dump($updatedMsg);
+// $updatedMsg = new Message();
+// $updatedMsg->setSubject("Updated Subject!");
+// $updatedMsg = $graphClient->usersById(USER_ID)->messagesById($sampleMessageId)->patch($updatedMsg)->wait();
+// echo "\n\n********************* PATCH ITEM ****************************\n\n";
+// var_dump($updatedMsg);
 
 // DELETE
+// $graphClient->usersById(USER_ID)->messagesById($sampleMessageId)->delete();
+// echo "\n\n Trying to feth DELETED message by Id: {$sampleMessageId}\n\n";
+// $message = $graphClient->usersById(USER_ID)->messagesById($sampleMessageId)->get()->wait();
 
-// WITH QUERY PARAMETERS
+// WITH QUERY PARAMETERS & HEADERS
+// $requestConfig = new MessagesRequestBuilderGetRequestConfiguration();
+// $requestConfig->queryParameters = new MessagesRequestBuilderGetQueryParameters();
+// $requestConfig->queryParameters->select = ['subject', 'from'];
+// $requestConfig->queryParameters->skip = 2;
+// $requestConfig->queryParameters->top = 3;
+// $requestConfig->headers = ['Prefer' => 'outlook.body-content-type=text']; 
 
-// WITH HEADERS
+// $messages = $graphClient->usersById(USER_ID)->messages()->get($requestConfig)->wait();
+// print_r($messages);
+// $sampledMessageId = 'AAMkAGU1MzAyZTlmLThkYTEtNGJmNC05Y2JhLWViMGQ1OTQ5NGIzOQBGAAAAAAC8WzGz8MUOR6pPeJq_p11LBwDDDW8iVoqYRqbHdJQE8vrTAAAAAAEMAADDDW8iVoqYRqbHdJQE8vrTAAA7E5niAAA=';
+
+
+// PLAIN TEXT DESERIALIZATION
+$numMessages = $graphClient->usersById(USER_ID)->messages()->count()->get()->wait();
+echo "Num messages: {$numMessages}";
+
+
